af18c98dffed46efb69e134ade27fe2f93b13cce
Eric Morrison <eric_m250@outlook.com>
2026-04-16 11:32:35 -0700

    set up new app/lib folder and created initializeNote function

---
+++ app/lib/InitializeNote.tsx
@@ -0,0 +1,30 @@
+
+import type { Note } from "@/lib/types/api"
+
+export const initializeNote = async(
+    title: string,
+    content: string,
+    notebookId: string
+) => {
+
+    try{
+        const res = await fetch('/api/notes', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({title, content, notebookId})
+        })
+
+        if(!res.ok){
+            throw new Error('Error creating note')
+        }
+
+        const parsed: {data: Note} = await res.json()
+        const newNote = parsed.data
+
+        return newNote 
+    } catch (e) {
+        console.error(e)
+    }
+}
