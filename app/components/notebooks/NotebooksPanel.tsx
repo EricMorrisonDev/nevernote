23f795750a93e9f8fab5e09c2f96c05b9180a9fb
Eric Morrison <eric_m250@outlook.com>
2026-04-21 09:22:46 -0700

    moved deletenotebook modal content to a seperate file

--- app/components/notebooks/NotebooksPanel.tsx
+++ app/components/notebooks/NotebooksPanel.tsx
@@ -7,6 +7,7 @@ import { initializeNote } from "@/app/lib/InitializeNote";
 import Image from "next/image";
 import { NotebooksMenu } from "./panelComponents/NotebooksMenu";
 import { StacksMenu } from "./panelComponents/StacksMenu";
+import { DeleteNotebookModal } from "./panelComponents/DeleteNotebookModal";
 
 interface NotebooksPanelProps {
     selectedNotebookId: string | null,
@@ -147,28 +148,16 @@ export function NotebooksPanel({
         switch (modalType) {
             case "delete":
                 return (
-                    <>
-                        <h4 id="delete-notebook-title">Are you sure you want to delete {notebookPendingDeletion?.title} ?</h4>
-                        <p>This will also delete all notes contained in the notebook.</p>
-                        <div className="flex justify-between mt-4">
-                            <button
-                            className="rounded-lg border border-red-400/30 bg-red-500/10 px-3 py-2 text-sm font-medium text-red-200 hover:bg-red-500/15 w-[110px]"
-                            onClick={() => {
-                                handleDeleteNotebook(notebookIdToBeDeleted)
-                            }}>
-                                Delete
-                            </button>
-                            <button
-                            className="rounded-lg border border-border bg-background px-3 py-2 text-sm font-medium text-foreground hover:bg-surface-2 w-[110px]"
-                            onClick={() => {
-                                setNotebookIdToBeDeleted(null)
-                                closeModal()
-                            }}
-                            >
-                                Cancel
-                            </button>
-                        </div>
-                    </>
+                    <DeleteNotebookModal
+                        notebookTitle={notebookPendingDeletion?.title}
+                        onConfirmDelete={() => {
+                            handleDeleteNotebook(notebookIdToBeDeleted)
+                        }}
+                        onCancel={() => {
+                            setNotebookIdToBeDeleted(null)
+                            closeModal()
+                        }}
+                    />
                 )
             case "create-notebook":
                 return (
